6e3d39d2fb5e14c287253e6ba65f8ecb915024f4
rubencodacy <ruben.santos@codacy.com>
2022-09-28 14:49:28 +0100

    add test1

---
+++ src/test/java/com/codacy/utils/Test1.java
@@ -0,0 +1,14 @@
+package com.codacy.utils;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Test1 {
+
+    @Test
+    public void test() {
+        int x = 1;
+        assertEquals(1, x);
+    }
+}
