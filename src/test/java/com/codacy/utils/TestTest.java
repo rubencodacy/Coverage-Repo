78eea1339ac15ca8bacf1caa5d92d7f467956a9a
rubencodacy <ruben.santos@codacy.com>
2022-09-28 14:33:27 +0100

    Add new test file

---
+++ src/test/java/com/codacy/utils/TestTest.java
@@ -0,0 +1,15 @@
+package com.codacy.utils;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestTest {
+
+    // Uncomment this to have 100% coverage
+    @Test
+    public void shouldSubtractIfMagicNumber() {
+        int x = 1;
+        assertEquals(1, x);
+    }
+}
