0db97edf156e0d15450cb0324ba1f13b170f1646
Rúben Santos <ruben.santos@codacy.com>
2023-03-09 15:48:51 +0000

    add 10000 lines file

---
+++ GenerateLines.scala
@@ -0,0 +1,33 @@
+
+
+def addLine(i: Int) = "\t\t\t\tSystem.out.println(\"hey\");\n" * i
+
+
+val file = s"""
+    |package com.codacy.utils;
+    |
+    |public class Math {
+    |
+    |    private int magicNumber;
+    |
+    |    public Math(int magicNumber) {
+    |        this.magicNumber = magicNumber;
+    |    }
+    |
+    |    /**
+    |     * Adds 2 numbers, unless there is a magic number on the second argument
+    |     */
+    |    public int magicAdd(int x, int y) {
+    |        if (y == magicNumber) {
+    |            ${addLine(10000)}
+    |            return y - x;
+    |        } else {
+    |            return x + y;
+    |        }
+    |    }
+    |
+    |
+    |}
+    |""".stripMargin
+
+os.write(os.pwd/"Math.java", file)
